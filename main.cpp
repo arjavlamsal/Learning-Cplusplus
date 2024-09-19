77d4f848e7ca118bb9ae179dc3fdf8513d2098ba
arjavlamsal <lamsalarjav@gmail.com>
2024-09-19 12:15:54 -0500

    string operations - append, add, find, at, erase, clear, insert

--- main.cpp
+++ main.cpp
@@ -2,23 +2,7 @@
 
 int main(){
     
-    std::string name;
-    std::cout << "Enter your name = ";
-    std::getline(std::cin, name);
-
-    // Clear the contents in the string variable name
-    // std:: cout << name.clear(); 
-
-    // Targeting position of character in string
-    std::cout << name.at(0) << '\n';
     
-    // Append method
-    std::cout << name.append("@gmail.com") << '\n';
-
-    std::cout << "Length = " << name.length() << '\n';
-    std::cout << name.empty();
     
-
-
     return 0;
 }
