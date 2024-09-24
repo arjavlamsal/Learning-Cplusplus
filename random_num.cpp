2fecc14c6561a62bef0c824d1786ae85050e3179
arjavlamsal <lamsalarjav@gmail.com>
2024-09-24 12:35:08 -0500

    guessing game

--- random_num.cpp
+++ random_num.cpp
@@ -1,20 +1,45 @@
+// Number guessing game.
 #include <iostream>
+#include <ctime>
 
 int main(){
+    using namespace std;
 
-    // Not truely random but close
+    int tries = 0;
+    int guess;
+    char usr_in;
+    
+    srand(time(0));
 
-    // Will have to put in seed for random number or won't work
-    srand(time(NULL));
+    int rand_num = (rand() % 10) + 1;
 
-    int num1 = (rand() % 20) + 1;
-    int num2 = (rand() % 20) + 1;
-    int num3 = (rand() % 20) + 1;
+    bool flag = true;
 
+    while (flag)
+    {
+        cout << "Do you want to play the game? (y/n)= ";
+        cin >> usr_in;
+        usr_in = tolower(usr_in); // Changed to lower case.
 
+         if (usr_in == 'y')
+         {
+            cout << "Enter a guess = ";
+            cin >> guess;
 
-    std::cout << num1;
+            if (guess == rand_num){
+                cout << "You guessed it!" << '\n';
+            } else {
 
+                cout << "Wrong! " << "The answer was " << rand_num << '\n' << "Better luck next time..." << '\n';
+            }
+         } else if (usr_in == 'n'){
+            cout << "Thank You for playing! See you again soon.." << '\n';
+            flag = false;
+         } else {
+            cout << "Enter correct response!" << '\n';
+         }
 
+    }
+    
     return 0;
 }
