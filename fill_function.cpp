c2a8bc9fee77e6923f7a3d0ce52341f91eaef594
arjavlamsal <lamsalarjav@gmail.com>
2024-09-28 11:48:41 -0500

    ofstream

--- fill_function.cpp
+++ fill_function.cpp
@@ -1,25 +1,15 @@
-// fill() function to initialize an array. fill(begin, end, value)
 #include <iostream>
+#include <fstream> // For file handling operations (ofstream & ifstream)
+
+using namespace std;
 
 int main(){
-    const int SIZE = 99;
-    std::string array[SIZE];
+    ofstream file1; // Initialize which file object for writing
+    file1.open("Text.txt"); // Open file for manipulation
 
-// For the initialization of the whole array.
-    // fill(array, array + SIZE, "pizza"); // starting address is the name of the array "array" and ending is upto array + size 
+    file1 << "Arjav Lamsal \n" << "Amrita Lamsal \n" << "Hari Lamsal \n" << endl;
 
-// Filling an array in 3 divisions.
-    // First 1/3 part.
-    fill(array, array + SIZE/3, "a");
-    // Second 1/3 part.
-    fill(array + SIZE/3, array + SIZE/3 * 2, "b");
-    // Third 1/3 part.
-    fill(array + SIZE/3 * 2, array + SIZE, "c");
+    file1.close();
 
-
-    for (std::string item : array){
-        std::cout << item << '\n';
-    }
-    
     return 0;
 }
